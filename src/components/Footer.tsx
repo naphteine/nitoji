2d10b05141fef329e603a6bb75ff29eefb1c4e41
gguilt <58526116+naphteine@users.noreply.github.com>
2023-02-09 21:01:20 +0300

    Added header and footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.footer}>
             Tüm hakları saklıdır. (c) 2022-2023. Nitoji
             <a href="https://gokaygultekin.dev">GG</a>
         </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
 import styles from "../styles/Header.module.css";
 
 const Header = () => {
     return (
-        <header>
-            日土辞書
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                日土辞書
+            </div>
+
+            <div className={styles.links}>
+                <Link className={styles.link} href="/giris">Giriş</Link>
+                <Link className={styles.link} href="/kayit">Kayıt</Link>
+            </div>
         </header>
     )
 }
