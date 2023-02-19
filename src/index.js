2642d64da92ab25c48a4931323474e8540d2f453
gguilt <58526116+naphteine@users.noreply.github.com>
2023-02-19 16:48:35 +0300

    Removed unnecessary files and updated styling

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ const router = createBrowserRouter([
         element: <Profile />,
       },
       {
-        path: "/profile/:id",
+        path: "/profil/:id",
         element: <UserProfile />
       }
     ],
