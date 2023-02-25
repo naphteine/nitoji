46becfaa4b88a6c2afaba6118bee71031d087cca
gguilt <58526116+naphteine@users.noreply.github.com>
2023-02-25 21:33:10 +0300

    Version 3 start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +15,43 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          日土辞書
+        </Link>
+      </header>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <div className={styles.captionList}>
+          <article className={styles.captionItem}>
+            <header>
+              <ruby>
+                漢<rt>かん</rt>
+              </ruby>
 
-      <Footer />
+              <ruby>
+                字<rt>じ</rt>
+              </ruby>
+            </header>
+            <div className={styles.badgeList}>
+              <span className={styles.badge}>jlpt n5</span>
+              <span className={styles.badge}>isim</span>
+              <span className={styles.badge}>常用</span>
+            </div>
+            <b>Türkçe okunuş:</b> Kanji
+            <h3>Çeviriler</h3>
+            <ol>
+              <li>Çin karakterleri</li>
+              <li>Han Hanedanlığı yazım sistemi</li>
+            </ol>
+          </article>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        Tüm hakları saklıdır. Nitoji &copy; 2022-2023.
+        <a href="https://www.gokaygultekin.dev">GG</a>
+      </footer>
     </>
   );
 }
