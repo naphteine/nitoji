01ae037d9957375343e33f6c9b1e476c31befc99
gguilt <58526116+naphteine@users.noreply.github.com>
2023-02-25 17:50:15 +0300

    Added old styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,65 +1,52 @@
-import Link from 'next/link';
-import { BiSearchAlt } from "react-icons/bi"
+import Link from "next/link";
+import { BiSearchAlt, BiMessageSquareAdd } from "react-icons/bi";
+import { RiAccountPinCircleFill } from "react-icons/ri";
+import { FaRegUserCircle } from "react-icons/fa";
+import { HiLogout } from "react-icons/hi";
 
-import styles from '@/styles/Header.module.css'
+import styles from "@/styles/Header.module.css";
 
 const Header = () => {
-    return (
-        <header className={styles.header}>
-            <Link href="/" className={styles.logo}>日土辞書</Link>
+  const jwtToken = "gg";
 
-            <nav>
-                <Link href="/konu" className={styles.link}>
-                    Konular
-                    <BiSearchAlt size={16} style={{ margin: 5 }} />
-                </Link>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        日土辞書
+      </Link>
+
+      <nav className={styles.nav}>
+        <Link href="/konu" className={styles.link}>
+          Konular
+          <BiSearchAlt size={16} className={styles.icon} />
+        </Link>
+
+        {jwtToken === "" ? (
+          <Link href="/giris" className={styles.link}>
+            Kayıt/Giriş
+            <FaRegUserCircle size={16} className={styles.icon} />
+          </Link>
+        ) : (
+          <>
+            <Link href="/profil" className={styles.link}>
+              Profil
+              <RiAccountPinCircleFill size={16} style={{ margin: 5 }} />
+            </Link>
+
+            <Link href="/dict" className={styles.link}>
+              Başlık aç
+              <BiMessageSquareAdd size={16} style={{ margin: 5 }} />
+            </Link>
+
+            <Link href="/l" className={styles.link}>
+              Çıkış
+              <HiLogout size={16} style={{ margin: 5 }} />
+            </Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
-
-/*
-
-            <Link className={styles["link"]} to="/konular">
-              Konular
-              
-            </Link>
-          </span>
-
-          <span className="text-end">
-            {jwtToken === "" ? (
-              <Link className={styles["link"]} to="/giris">
-                Kayıt/Giriş
-                <FaRegUserCircle size={16} style={{ margin: 5 }} />
-              </Link>
-            ) : (
-              <>
-                <Link className={styles["link"]} to="/yeni">
-                  Başlık aç
-                  <BiMessageSquareAdd size={16} style={{ margin: 5 }} />
-                </Link>
-
-                <Link to="/profil" className={styles["link"]}>
-                  Profil
-                  <RiAccountPinCircleFill size={16} style={{ margin: 5 }} />
-                </Link>
-
-                <Link to="/" onClick={logOut} className={styles["link"]}>
-                  Çıkış
-                  <HiLogout size={16} style={{ margin: 5 }} />
-                </Link>
-              </>
-            )}
-
-            {jwtToken && jwt(jwtToken).role && jwt(jwtToken).role === "mod" && (
-              <Link to="/mod" className={styles["link"]}>
-                <b>Mod Paneli</b>
-                <HiAdjustments size={16} style={{ margin: 5 }} />
-              </Link>
-            )}
-          </span>
-        </nav>
-      </header>
-*/
