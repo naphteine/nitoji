53816898b5d84c02708c76f33c8841a781cc4567
gguilt <58526116+naphteine@users.noreply.github.com>
2023-02-28 18:19:42 +0300

    Separated data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,59 @@ import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const dictData = [
+    {
+      id: 0,
+      kanji: true,
+      ruby: [
+        {
+          id: 0,
+          body: "日",
+          text: "にち",
+        },
+      ],
+      reading: "nichi",
+      tags: ["jlpt n5", "常用", "grade 1"],
+      kun: ["ひ", "-び", "-か"],
+      on: ["ニチ", "ジツ"],
+      tr: ["Pazar günü", "Japonya", "Gün sayacı", "Ayın n'inci günü"],
+    },
+    {
+      id: 1,
+      kanji: true,
+      ruby: [
+        {
+          id: 0,
+          body: "漢",
+          text: "かん",
+        },
+      ],
+      reading: "kan",
+      tags: ["jlpt n4", "常用", "grade 3"],
+      on: ["カン"],
+      tr: ["Çin", "Han Hanedanlığı (M.Ö. 202 - M.S. 220)"],
+    },
+    {
+      id: 2,
+      kanji: false,
+      ruby: [
+        {
+          id: 0,
+          body: "漢",
+          text: "かん",
+        },
+        {
+          id: 1,
+          body: "字",
+          text: "じ",
+        },
+      ],
+      reading: "Kanji",
+      tags: ["jlpt n5", "isim", "常用"],
+      tr: ["Çin karakterleri", "Han Hanedanlığı yazım sistemi"],
+    },
+  ];
+
   return (
     <>
       <Head>
@@ -23,73 +76,60 @@ export default function Home() {
 
       <main className={styles.main}>
         <div className={styles.captionList}>
-          <article className={styles.captionItem}>
-            <header title="Kanji">
-              <ruby>
-                漢<rt>かん</rt>
-              </ruby>
+          {dictData.map((dict) => {
+            return (
+              <article
+                key={dict.id}
+                className={dict.kanji ? styles.kanjiItem : styles.captionItem}
+              >
+                <header title={dict.reading}>
+                  {dict.ruby.map((ruby) => {
+                    return (
+                      <ruby key={ruby.id}>
+                        {ruby.body}
 
-              <ruby>
-                字<rt>じ</rt>
-              </ruby>
-            </header>
-            <div className={styles.badgeList}>
-              <span className={styles.badge}>jlpt n5</span>
-              <span className={styles.badge}>isim</span>
-              <span className={styles.badge}>常用</span>
-            </div>
-            <b>Türkçe okunuş:</b> Kanji
-            <ol>
-              <li>Çin karakterleri</li>
-              <li>Han Hanedanlığı yazım sistemi</li>
-            </ol>
-          </article>
+                        <rt>{ruby.text}</rt>
+                      </ruby>
+                    );
+                  })}
+                </header>
+                <div>
+                  <div className={styles.badgeList}>
+                    {dict.tags.map((tag) => {
+                      return (
+                        <span key={tag} className={styles.badge}>
+                          {tag}
+                        </span>
+                      );
+                    })}
+                  </div>
+                  {dict.kun && (
+                    <span>
+                      <b>Kun: </b>
+                      {dict.kun.map((kuns) => {
+                        return <span key={kuns}>{kuns} </span>;
+                      })}
+                    </span>
+                  )}
 
-          <article className={styles.kanjiItem}>
-            <header>
-              <ruby>
-                漢<rt>かん</rt>
-              </ruby>
-            </header>
-            <div>
-              <div className={styles.badgeList}>
-                <span className={styles.badge}>jlpt n4</span>
-                <span className={styles.badge}>常用</span>
-                <span className={styles.badge}>grade 3</span>
-              </div>
-              <b>On:</b> <span title="kan">カン</span>
-              <ol>
-                <li>Çin</li>
-                <li>Han Hanedanlığı (M.Ö. 202 - M.S. 220)</li>
-              </ol>
-            </div>
-          </article>
+                  {dict.on && (
+                    <span>
+                      <b>On: </b>
+                      {dict.on.map((ons) => {
+                        return <span key={ons}>{ons} </span>;
+                      })}
+                    </span>
+                  )}
 
-          <article className={styles.kanjiItem}>
-            <header>
-              <ruby>
-                日<rt>にち</rt>
-              </ruby>
-            </header>
-            <div>
-              <div className={styles.badgeList}>
-                <span className={styles.badge}>jlpt n5</span>
-                <span className={styles.badge}>常用</span>
-                <span className={styles.badge}>grade 1</span>
-              </div>
-              <b>Kun</b>: <span title="hi">ひ</span>、{" "}
-              <span title="-bi">-び</span>、 <span title="-ka">-か</span>
-              <br />
-              <b>On:</b> <span title="nichi">ニチ</span>、{" "}
-              <span title="jitsu">ジツ</span>
-              <ol>
-                <li>Pazar günü</li>
-                <li>Japonya</li>
-                <li>Gün sayacı</li>
-                <li>Ayın n&apos;inci günü</li>
-              </ol>
-            </div>
-          </article>
+                  <ol>
+                    {dict.tr.map((translation) => {
+                      return <li key={translation}>{translation}</li>;
+                    })}
+                  </ol>
+                </div>
+              </article>
+            );
+          })}
         </div>
       </main>
 
