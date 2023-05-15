550201dc68e54c989b4feef2d045cbd6a6796d6d
gguilt <58526116+naphteine@users.noreply.github.com>
2023-05-15 21:07:19 +0300

    Initial PocketBase version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,96 +1,25 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
+import PocketBase, { BaseModel, ListResult, Record } from "pocketbase";
 
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const initDictData = [
-    {
-      id: 0,
-      kanji: true,
-      ruby: [
-        {
-          id: 0,
-          body: "日",
-        },
-      ],
-      reading: "nichi",
-      tags: ["jlpt n5", "常用", "grade 1"],
-      kun: ["ひ", "-び", "-か"],
-      on: ["ニチ", "ジツ"],
-      tr: ["Pazar günü", "Japonya", "Gün sayacı", "Ayın n'inci günü"],
-    },
-    {
-      id: 1,
-      kanji: true,
-      ruby: [{ body: "漢" }],
-      tags: ["jlpt n4", "常用", "grade 3"],
-      on: ["カン"],
-      tr: ["Çin", "Han Hanedanlığı (M.Ö. 202 - M.S. 220)"],
-    },
-    {
-      id: 2,
-      kanji: false,
-      ruby: [
-        {
-          id: 0,
-          body: "漢",
-          text: "かん",
-        },
-        {
-          id: 1,
-          body: "字",
-          text: "じ",
-        },
-      ],
-      reading: "Kanji",
-      tags: ["jlpt n5", "isim", "常用"],
-      tr: ["Çin karakterleri", "Han Hanedanlığı yazım sistemi"],
-    },
-    {
-      id: 3,
-      kanji: false,
-      ruby: [
-        {
-          id: 0,
-          body: "明",
-          text: "あ",
-        },
-        {
-          id: 1,
-          body: "日",
-          text: "した",
-        },
-      ],
-      reading: "Ashita",
-      tags: ["jlpt n5", "isim", "常用"],
-      tr: ["Yarın"],
-    },
-    {
-      id: 4,
-      kanji: true,
-      ruby: [{ body: "明" }],
-      tags: ["kanji", "jlpt n4", "常用"],
-      tr: ["Aydınlık", "ışık"],
-      kun: [
-        "あ.かり",
-        " あか.るい",
-        " あか.るむ",
-        " あか.らむ",
-        " あき.らか",
-        " あ.ける",
-        " -あ.け",
-        " あ.く",
-        " あ.くる",
-        " あ.かす",
-      ],
-      on: ["メイ", "ミョウ", "ミン"],
-    },
-  ];
+  const [userData, setUserData] = useState<ListResult>();
 
-  const [dictData, setDictData] = useState(initDictData);
+  async function getAll() {
+    const pb = new PocketBase("http://127.0.0.1:8090");
+    const resultList = await pb.collection("dict").getList(1, 50, {
+      filter: "",
+    });
+
+    setUserData(resultList);
+  }
+
+  useEffect(() => {
+    getAll();
+  }, []);
 
   return (
     <>
@@ -106,7 +35,15 @@ export default function Home() {
         </Link>
       </header>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        {userData &&
+          userData.items &&
+          userData.items.map((e) => (
+            <div key={e.id}>
+              {e.japanese} ({e.jlpt}) - {e.turkish}
+            </div>
+          ))}
+      </main>
 
       <footer className={styles.footer}>
         Tüm hakları saklıdır. Nitoji &copy; 2022-2023.
