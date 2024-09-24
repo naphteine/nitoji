59e309705795646963d1b82462d6cb604a52dfca
naphteine <58526116+naphteine@users.noreply.github.com>
2024-09-25 00:33:59 +0300

    basic styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,22 @@ export default function Home() {
   return (
     <>
       <header>
-        <h1>Nitoji Project</h1>
+        <h1>
+          <Link href="/">Nitoji Project</Link>
+        </h1>
 
         <nav>
-          <Link href="/">Ana sayfa</Link> | <Link href="/dict">Sözlük</Link> |{" "}
-          <Link href="/resources">Kaynaklar</Link>
+          <Link href="/">Sözlük</Link> | <Link href="/news">Haberler</Link> |{" "}
+          <Link href="/">Dil ve Kültür</Link> |{" "}
+          <Link href="/resources">Kaynaklar</Link> |{" "}
+          <Link href="furiyomi.gokay.works">Furiyomi</Link>
         </nav>
       </header>
-      <main></main>
-      <footer>
+      <main className="my-20 text-center">
+        <input type="text" name="search" id="search" />
+        <input type="button" value="Ara" />
+      </main>
+      <footer className="text-center">
         Nitoji (c) 2022-2024 <a href="https://gokay.works">Gökay Gültekin</a> |{" "}
         <a href="https://github.com/naphteine/nitoji">Source</a>
       </footer>
